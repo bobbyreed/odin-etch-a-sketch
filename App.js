b85c5a6f526ca56f0c36195ad05563be75bec698
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-02-19 14:18:07 -0600

    Basic hover paint logic built and functional.

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
-const gridWidth = 16;
-const gridHeight = 16;
+const container = document.querySelector("#container");
+//container.addEventListener("mouseenter", hoverTest());
 
-for(let i = 0; i < gridWidth; i++){
-    document.createElement("div");
-    document.appendChild("")
+if(container == null){
+    console.log("container is null");
 }
+
+createDivs(16);
+
+function createDivs(num){
+    for(let i = 0; i < num; i++){
+            let content = document.createElement("div");
+            content.classList.add("grid"); 
+            content.addEventListener('mouseenter', (event) => hoverFill(content));
+            container.appendChild(content);            
+        for(let j = 0; j < num; j++){
+            let content = document.createElement("div");
+            content.classList.add("grid");
+            content.addEventListener('mouseenter', (event) => hoverFill(content));
+            container.appendChild(content);
+        }
+        }
+    }
+
+function hoverFill(div){
+    //console.log("coloring");
+    div.classList.remove("grid");
+    div.classList.add("grid-colored");
+}
+
+
